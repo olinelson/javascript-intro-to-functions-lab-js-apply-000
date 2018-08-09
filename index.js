f91526e5dd1cf9886968f1ae90ba85278300e7b0
Oliver Nelson <olinelson93@gmail.com>
2018-08-09 19:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   string.toLowerCase() === string // true
   string.toUpperCase() === string // false;
-  ;true return "I can\'t hear you!";
-  false return "YES INDEED"
+  true; return "I can\'t hear you!";
+  false; return "YES INDEED"
  
 }
 
